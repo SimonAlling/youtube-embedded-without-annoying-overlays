bb8c5cb9968934e043dff98c2c99afb1a1a831ac
Simon Alling <alling.simon@gmail.com>
2020-04-14 17:49:21 +0200

    Add the userscript

---
+++ main.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         YouTube Embedded Without Annoying Overlays
+// @namespace    simonalling.se
+// @version      1.0.0
+// @author       Simon Alling
+// @match        *://*.youtube.com/*
+// @description  Hides "More videos" etc in embedded YouTube videos.
+// @run-at       document-start
+// ==/UserScript==
+
+(() => {
+"use strict";
+
+const ELEMENTS_TO_HIDE = [
+    ".ytp-pause-overlay", // More videos
+    ".ytp-player-content.ytp-iv-player-content", // Uploader avatar in bottom right corner
+];
+
+const CSS = `
+${ELEMENTS_TO_HIDE.join(", ")} {
+    display: none !important;
+}
+`;
+
+const styleElement = document.createElement("style");
+styleElement.textContent = CSS;
+document.head.appendChild(styleElement);
+
+})();
